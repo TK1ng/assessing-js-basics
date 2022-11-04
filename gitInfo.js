8cc18bd1b2ab762ea8733460430117b9b4358149
Taryn King <marshaeking@gmail.com>
2022-11-04 15:02:33 -0500

    Create variables to store key git command definitions

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+const gitDefinition = 'Git is an open source version control software developed by Linus Torvalds. It helps to track revision history in code projects and makes it easier for teams of developers to collaborate';
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+const gitHubDefinition = 'Github is a cloud-based service that uses git to help users maintain code and practice version control.';
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+const gitInitDefinition = 'git init is a git command that is used to initialize git within the directory where the command is run.';
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+const gitCloneDefinition = 'The git clone command makes a duplicate of a specified repository in a new directory.';
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+const gitStatusDefinition = 'The git status command shows the current state of the working tree and staging area. This allows the developer to see if the local repo is up to date with the remote repo and if there are untracked files.';
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,8 @@
 */
 
 //CODE HERE
-
+const gitAddDefinition = 'The git add command adds new or changed files in the working tree to the staging area.';
+const gitAddCode = 'git add . ';
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +65,7 @@
 */
 
 //CODE HERE
-
+const gitCommitDefinition = 'The git commit command creates a snapshot to capture changes made to files in a repository';
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +74,4 @@
 */
 
 //CODE HERE
+const gitPushDefinition = 'git push uploads commits from the local branch of a repository to the remote repository';
