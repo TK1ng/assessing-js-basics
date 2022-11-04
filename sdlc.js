ae9616f0c667ceb761ff1d701835703fe9fef099
Taryn King <marshaeking@gmail.com>
2022-11-04 14:08:51 -0500

    Define development lifecycle and required variables

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,10 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+
+const analysisOfRequirements = "This is the second step which involves gathering details to understand the technical requirements and what is required by users and stakeholders";
+const design = "This is the step where a design is created to map out how the software will look and how it will function";
+const implementation = "Software is built in this step and designs are put into code";
+const testingAndIntegration = "Testing takes place in this step to make sure the application or software works as designed with the expected functionality.";
+const maintain = "This step is about maintenance of the application or software, ensuring that it continues to work and is updated where need.";
