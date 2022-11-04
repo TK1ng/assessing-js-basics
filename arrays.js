9fc216a2db8cb2620837ec75a0eaeb7b3c19d560
Taryn King <marshaeking@gmail.com>
2022-11-04 14:38:39 -0500

    Solve array problem set

--- arrays.js
+++ arrays.js
@@ -4,11 +4,17 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice(0);
+
+// console.log(colorCopy);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('indigo');
+
+// console.log(colorCopy);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +25,9 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+const middleNums = numbers.slice(1, 4);
 
+// console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +42,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] <= 100) {
+    answers.push('small');
+  } else {
+    answers.push('big');
+  }
+}
+
+// console.log(answers)
